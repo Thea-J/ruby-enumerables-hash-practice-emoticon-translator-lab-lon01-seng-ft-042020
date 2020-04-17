0f2114ebf281f4cc8d67673b04de07bb43e0a0cb
Dorothy John <dorothyy.john@gmail.com>
2020-04-17 09:27:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,7 +11,7 @@ emoticons = YAML.load_file(file_path)
   emoticons.each do |emoji_name, emoji_array|
     emoticon_hash[:emoji_name] = { }
     emoticon_hash[:emoji_name][:english] = emoji_array[0]
-    emoticon_hash[:emoji_name][:jap] = emoji_array[1]
+    emoticon_hash[:emoji_name][:japanese] = emoji_array[1]
   end
   emoticon_hash
 end
