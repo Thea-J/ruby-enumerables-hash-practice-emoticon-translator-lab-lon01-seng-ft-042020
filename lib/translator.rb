5f184ca317dd605ac926189360fd4bbe8dc3915b
Dorothy John <dorothyy.john@gmail.com>
2020-04-17 10:42:53 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -39,7 +39,7 @@ def get_english_meaning(file_path, japanese_emoticon)
   
    emoticon_translation = emoticon_hash.each do |emoji_name, emoji_hash|
   if emoji_hash[:japanese] == japanese_emoticon
-   emoticon_hash[emoji_name]
+   p "#{emoji_name}"
   else 
   "Sorry, that emoticon was not found"
 end
