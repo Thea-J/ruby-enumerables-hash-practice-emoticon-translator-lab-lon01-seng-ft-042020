8c77f4c98216141f47f66bfbf0857a994c059eec
Dorothy John <dorothyy.john@gmail.com>
2020-04-17 09:40:38 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,8 +16,9 @@ emoticons = YAML.load_file(file_path)
   emoticon_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon (file_path, japanese_emoticon)
+  load_library(file_path)
+  
 end
 
 def get_english_meaning
